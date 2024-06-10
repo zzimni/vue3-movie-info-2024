4f5a5185d2fbc8d929323495acf3df13bbca6545
zzimni <hjm3327@naver.com>
2024-06-10 16:48:26 +0900

    1-6 강의노트 커밋

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,16 @@
     <h3 class="bg-yellow">{{ movie.title }}</h3>
     <p>개봉: {{ movie.year }}</p>
     <p>장르: {{ movie.category }}</p>
-    <button 
-      @:click="increseLike(i)">좋아요
-    </button> 
+    <button @:click="increseLike(i)">좋아요</button>
+    <!-- v-on:이벤트명="실행코드(함수)"-->
+    <!-- v-on은 @로 대체해서 쓸 수 있음 -->
     <span>{{ movie.like }}</span>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App',
+  name: "App",
   data() {
     return {
       data: [
@@ -27,24 +27,24 @@ export default {
         {
           title: "아쿠아맨과 로스트 킹덤",
           year: 2023,
-          category: '액션, 판타지, 어드벤처',
+          category: "액션, 판타지, 어드벤처",
           like: 0,
         },
         {
           title: "3일의 휴가",
           year: 2023,
-          category: '판타지, 드라마',
+          category: "판타지, 드라마",
           like: 0,
         },
-      ]
-    }
+      ],
+    };
   },
   methods: {
     increseLike(i) {
-      this.data[i].like += 1;
-    }
-  }
-}
+      this.data[i].like += 1; // 객체 내부에서 사용하는 변수는 앞에 this.를 붙여준다
+    },
+  },
+};
 </script>
 
 <style>
