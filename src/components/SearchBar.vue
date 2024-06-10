098e2df5239e448a571ece88f07da621ffab9d03
zzimni <hjm3327@naver.com>
2024-06-10 23:08:22 +0900

    1-15 강의노트 커밋

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,38 +1,35 @@
 <template>
   <div class="search-box">
-    <input 
-      type="search" 
-      v-model="inputText"
-      placeholder="검색어 입력"
-    >
+    <input type="search" v-model="inputText" placeholder="검색어 입력" />
+    <!-- v-model="변수" : 사용자에게 입력받은 값을 변수에 알아서 저장해준다 -->
     <button>검색</button>
   </div>
   <p>{{ inputText }}</p>
 </template>
-  
+
 <script>
 export default {
   name: "SearchBarComponent",
   data() {
     return {
-      inputText: '',
-    }
-  }
-}
+      inputText: "",
+    };
+  },
+};
 </script>
-  
+
 <style>
-  .search-box {
-    padding: 10px;
-    display: flex;
-    justify-content: center;
-  }
+.search-box {
+  padding: 10px;
+  display: flex;
+  justify-content: center;
+}
 
-  .search-box input {
-    padding: 5px 10px;
-  }
+.search-box input {
+  padding: 5px 10px;
+}
 
-  .search-box button {
-    margin: 0;
-  }
+.search-box button {
+  margin: 0;
+}
 </style>
