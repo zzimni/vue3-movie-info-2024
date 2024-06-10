e4c176f51c5778b48951b1e1beb5525af39a8166
zzimni <hjm3327@naver.com>
2024-06-10 16:40:44 +0900

    1-5 강의 노트 커밋

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <h1>영화정보</h1>
   <div v-for="(movie, i) in data" :key="i">
-    <h3 class="bg-yellow">{{ movie.title }}</h3>
+    <!-- v-for ="(item,index) in 데이터"-->
+    <!-- :key="키값"-->
+    <h3 class="bg-yellow" :style="textRed">{{ movie.title }}</h3>
+    <!-- :속성명 = "데이터" -->
     <p>개봉: {{ movie.year }}</p>
     <p>장르: {{ movie.category }}</p>
   </div>
@@ -9,30 +12,31 @@
 
 <script>
 export default {
-  name: 'App',
+  name: "App", // 컴포넌트 명 여기에 적기
   data() {
+    // 문서에 표시될 변수들 정의 - 상태 변수 (자주 변경되는 값)을 여기에 정의
     return {
+      textRed: "color: red",
       data: [
         {
           title: "노량",
           year: 2023,
           category: "액션, 드라마",
-          textRed: "color: red",
         },
         {
           title: "아쿠아맨과 로스트 킹덤",
           year: 2023,
-          category: '액션, 판타지, 어드벤처',
+          category: "액션, 판타지, 어드벤처",
         },
         {
           title: "3일의 휴가",
           year: 2023,
-          category: '판타지, 드라마',
+          category: "판타지, 드라마",
         },
-      ]
-    }
-  }
-}
+      ],
+    };
+  },
+};
 </script>
 
 <style>
